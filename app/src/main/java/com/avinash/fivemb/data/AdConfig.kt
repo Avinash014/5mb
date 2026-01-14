5c38262c019cf281a6bb8e750f50056cb660d327
Avinash kumar <avinash.kumar@cognizer.com>
2026-01-14 15:51:53 +0530

    Enabling only the Ad on Stats Screen

--- app/src/main/java/com/avinash/fivemb/data/AdConfig.kt
+++ app/src/main/java/com/avinash/fivemb/data/AdConfig.kt
@@ -1,7 +1,7 @@
 package com.avinash.fivemb.data
 
 object AdConfig {
-    const val SHOW_QUIZ_BANNER = true
+    const val SHOW_QUIZ_BANNER = false // currently making it false for enhance UI
     const val SHOW_STATS_BANNER = true
     
     // Future expansion: Load this from a remote config or JSON
